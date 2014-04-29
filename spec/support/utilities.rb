30741b3a48895046c8383684e460bc5e8ed6c5c8
Jamie <jxberc@gmail.com>
2014-04-29 10:03:36 -0400

    add comment to spec/support/utilities

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,6 @@
+# no longer use after writing test cases for full_title method in
+# app/helpers/application_helper.rb
+
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
