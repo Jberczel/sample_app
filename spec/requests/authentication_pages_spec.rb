91f6192c6b0f20ab56fb90992f8bd89142811b3d
Jamie <jxberc@gmail.com>
2014-05-19 08:41:42 -0400

    finish ch. 9 exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,6 +7,15 @@ describe "Authentication" do
   describe "signin" do
     before { visit signin_path }
 
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+
+    it { should_not have_link('Users') }
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
+    it { should_not have_link('Sign out', href: signout_path) }
+    it { should have_link('Sign in', href: signin_path) }
+
     describe "with invalid information" do
       before { click_button "Sign in" }
 
@@ -22,9 +31,7 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        sign_in user
       end
 
       it { should have_title(user.name) }
@@ -43,6 +50,21 @@ describe "Authentication" do
   end
 
   describe "authorization" do
+
+    describe "as signed-in user " do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user, no_capybara:true }
+
+      describe "cannot access #new action" do
+        before { get new_user_path }
+        specify { response.should redirect_to(root_path) }
+      end
+
+      describe "cannot access #create action" do
+        before { post users_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
 
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
@@ -80,9 +102,7 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in user
         end
 
         describe "after signing in" do
@@ -90,9 +110,22 @@ describe "Authentication" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
+
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
+          end       
         end
       end
-
     end
 
     describe "as wrong user" do
@@ -112,6 +145,19 @@ describe "Authentication" do
       end
     end
   end
+
+  describe "as admin user" do
+    let(:admin) { FactoryGirl.create(:admin) }
+    before { sign_in admin, no_capybara: true }
+
+    describe "should not be able to delete themselves via #destroy action" do
+      specify do
+        expect { delete user_path(admin) }.not_to change(User, :count).by(-1)
+      end
+    end
+  end
+
+
 
 end
 
